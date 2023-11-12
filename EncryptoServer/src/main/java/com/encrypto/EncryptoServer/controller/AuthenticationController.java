0f4525f51acf4199d1ed26f4f0070f763bb925d1
Rohit Prakash <rohitp934@gmail.com>
2023-11-11 19:38:56 -0500

    Add DTO for responses

--- EncryptoServer/src/main/java/com/encrypto/EncryptoServer/controller/AuthenticationController.java
+++ EncryptoServer/src/main/java/com/encrypto/EncryptoServer/controller/AuthenticationController.java
@@ -1,9 +1,12 @@
 package com.encrypto.EncryptoServer.controller;
 
+import com.encrypto.EncryptoServer.dto.response.RegisterResponse;
 import com.encrypto.EncryptoServer.model.Users;
 import com.encrypto.EncryptoServer.repository.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,11 +24,12 @@ public class AuthenticationController {
     private PasswordEncoder passwordEncoder;
 
     @PostMapping("/register")
-    public String signUp(@Valid @RequestBody Users user) {
+    public ResponseEntity<?> signUp(@Valid @RequestBody Users user) {
         // Log the user request body.
         System.out.println("Username: " + user.getUsername() + "\nPassword: " + user.getPassword() + "\nDate of birth: " + user.getDateOfBirth());
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
-        return "User registered successfully";
+        RegisterResponse res = new RegisterResponse(true, "User registered successfully");
+        return ResponseEntity.status(HttpStatus.CREATED).body(res);
     }
 }
