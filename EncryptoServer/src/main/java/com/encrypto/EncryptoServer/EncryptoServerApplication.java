59452e200e8712b203a1dcb7eb0b754d051989d7
Rohit Prakash <rohitp934@gmail.com>
2023-11-11 19:38:12 -0500

    Default UTC

--- EncryptoServer/src/main/java/com/encrypto/EncryptoServer/EncryptoServerApplication.java
+++ EncryptoServer/src/main/java/com/encrypto/EncryptoServer/EncryptoServerApplication.java
@@ -1,15 +1,24 @@
 package com.encrypto.EncryptoServer;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static java.util.TimeZone.getTimeZone;
+import static java.util.TimeZone.setDefault;
 
 @SpringBootApplication
 public class EncryptoServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EncryptoServerApplication.class, args);
-		// Print listening to console
-		System.out.println("Listening...");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EncryptoServerApplication.class, args);
+        // Print listening to console
+        System.out.println("Listening...");
+    }
 
+    @PostConstruct
+    public void init() {
+        // Set default timezone to UTC
+        setDefault(getTimeZone("UTC"));
+    }
 }
