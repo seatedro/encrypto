010a461fbd14ce2230f249317720f028c71e2318
Rohit Prakash <rohitp934@gmail.com>
2023-11-11 19:38:28 -0500

    Default Exception Handler

---
+++ EncryptoServer/src/main/java/com/encrypto/EncryptoServer/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.encrypto.EncryptoServer.exception;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(BAD_REQUEST)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        DefaultMessageSourceResolvable::getDefaultMessage
+                ));
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
--- EncryptoServer/src/main/java/com/encrypto/EncryptoServer/exception/GlobalExecutionHandler.java
+++
@@ -1,2 +0,0 @@
-package com.encrypto.EncryptoServer.exception;public class GlobalExecutionHandler {
-}
