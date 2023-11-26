14acb80624ae270b59badce1dd48ff66c1b0dd98
Rohit Prakash <rohitp934@gmail.com>
2023-11-26 16:25:32 -0500

    Remove unnecessary files

--- EncryptoServer/src/main/java/com/encrypto/EncryptoServer/config/WebSocketConfiguration.java
+++ EncryptoServer/src/main/java/com/encrypto/EncryptoServer/config/WebSocketConfiguration.java
@@ -1,6 +1,5 @@
 package com.encrypto.EncryptoServer.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,7 +9,6 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
-    @Autowired CustomWebsocketUserHandler customHandler;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -21,6 +19,6 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/encrypto").setHandshakeHandler(customHandler).setAllowedOrigins("*");
+        registry.addEndpoint("/encrypto").setAllowedOrigins("*");
     }
 }
