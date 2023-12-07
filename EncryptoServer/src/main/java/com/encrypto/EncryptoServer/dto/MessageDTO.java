f10fc23acefd331a9ccd67e4d67f79b6b52b0296
Rohit Prakash <rohitp934@gmail.com>
2023-12-07 18:43:25 -0500

    Fixes?

--- EncryptoServer/src/main/java/com/encrypto/EncryptoServer/dto/MessageDTO.java
+++ EncryptoServer/src/main/java/com/encrypto/EncryptoServer/dto/MessageDTO.java
@@ -1,14 +1,10 @@
 package com.encrypto.EncryptoServer.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.Instant;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class MessageDTO {
